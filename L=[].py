a522a03cd75a1239f08dc50a6b99af946db8c34b
tanmay0910 <92428144+tanmay0910@users.noreply.github.com>
2021-11-18 11:22:01 +0530

    Add files via upload

---
+++ L=[].py
@@ -0,0 +1,17 @@
+L=[]
+T,S=0,0
+R,C=0,0
+P=1
+R=int(input("Enter number of rows"))
+C=int(input("Enter number of coloums"))
+print("Enter elements")
+for i in range(0, R, 1):
+    Rows= []
+for j in range(0,C,1):
+    T=int(input())
+    Rows.append(T)
+    2
+    P=P*Rows[j]
+L.append(Rows)
+print("Product is:",P) 
+P=1
